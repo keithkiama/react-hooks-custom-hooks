eaea09a70e4cbbb4e0b19c376c6d314e3e7076d2
keithkiama <keithkiama2003@gmail.com>
2023-01-22 14:35:16 +0300

    Completed Assignment

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
 
+
 function HomePage() {
   // fetch data for posts
   const [isLoaded, setIsLoaded] = useState(false);
